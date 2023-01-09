d1941d882c3b81d978d5eaa532fadd8c7c0d7b92
rodriiFdz <52882787+rodriiFdz@users.noreply.github.com>
2023-01-09 10:40:17 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,100 +1,177 @@
 
-//Variable carrito
-const carrito  =[]; 
+
+let carrito = []; 
 
 //Productos disponibles
-
 const productos = [
-  {
-    id: 1, 
-    nombre: 'Chaqueta Vintage amarilla', 
-    precio: 72.00, 
-  }, 
-  {
-    id: 2, 
-    nombre: 'Cardigan Vintage', 
-    precio: 25.00, 
-  },
-  {
-    id: 3, 
-    nombre: 'Chaqueta Vintage negra', 
-    precio: 50.00, 
-  },
-  {
-    id: 4, 
-    nombre: 'Abrigo Vintage', 
-    precio: 35.00, 
-  },
-  {
-    id: 5, 
-    nombre: 'Pantalón Vintage 80s', 
-    precio: 40.00, 
-  },
-  {
-    id: 6, 
-    nombre: 'Pantalón Vintage 90s', 
-    precio: 15.00, 
-  },
-  {
-    id: 7, 
-    nombre: 'Pantalón Vintage azul', 
-    precio: 15.00, 
-  },
-  {
-    id: 8, 
-    nombre: 'Pantalón Vintage rosa', 
-    precio: 18.00, 
-  },
-  {
-    id: 9, 
-    nombre: 'Zapatillas de lona', 
-    precio: 119.99, 
-  },
-  {
-    id: 10, 
-    nombre: 'Deportivas', 
-    precio: 99.99, 
-  },
-  {
-    id: 11, 
-    nombre: 'Botas', 
-    precio: 145.00, 
-  },
-  {
-    id: 12, 
-    nombre: 'Zapatos', 
-    precio: 240.00, 
-  },
-  
+  {id: 1, nombre: 'Chaqueta Vintage amarilla',  precio: 72.00, img: 'imagenes/Chaqueta.jpg', cantidad:1}, 
+  {id: 2,nombre: 'Cardigan Vintage', precio: 25.00, img: 'imagenes/Prod2.jpg' , cantidad:1},
+  {id: 3,nombre: 'Chaqueta Vintage negra', precio: 50.00, img: 'imagenes/Prod7.jpg', cantidad:1},
+  {id: 4, nombre: 'Abrigo Vintage', precio: 35.00, img: 'imagenes/Prod4.jpg', cantidad:1},
+  {id: 5, nombre: 'Pantalón Vintage 80s',precio: 40.00, img: 'imagenes/Prod3.jpg', cantidad:1},
+  {id: 6, nombre: 'Pantalón Vintage 90s', precio: 15.00, img: 'imagenes/Prod4.jpg', cantidad:1},
+  {id: 7, nombre: 'Pantalón Vintage azul', precio: 15.00, img: 'imagenes/Prod9.jpg', cantidad:1},
+  {id: 8, nombre: 'Pantalón Vintage rosa',precio: 18.00, img: 'imagenes/Prod10.jpg', cantidad:1},
+  {id: 9, nombre: 'Zapatillas de lona', precio: 119.99, img: 'imagenes/Prod5.jpg', cantidad:1},
+  {id: 10, nombre: 'Deportivas', precio: 99.99, img: 'imagenes/Prod6.jpg', cantidad:1},
+  {id: 11, nombre: 'Botas', precio: 145.00, img: 'imagenes/Prod11.jpg', cantidad:1},
+  {id: 12, nombre: 'Zapatos', precio: 240.00, img: 'imagenes/Prod12.jpg', cantidad:1},
 ]
 
+function carritoVacio(){
+  var cuerpoModal = document.getElementById("cuerpo-modal"); 
+  if(carrito.length === 0){ 
+    cuerpoModal.innerHTML = '<p> Su cesta esta vacía</p>'
 
+  }
+}
 
+function buscarProductoenCarrito(id){
+  var enCarrito = false; 
+  for(let i = 0; i < carrito.length; i++){
+    const prod = carrito[i]; 
+    if(prod.id === id){
+      enCarrito = true;  
+    }
+  }
 
+  return enCarrito;
+  
+}
+
+/* TODO añadir un subtotal a la línea e incluir descuentos */
 
 function addCarrito(id){
-    var tbody = document.getElementById("cuerpoTabla");
-    var filaProducto = '<tr><td>' + nombre + '</td><td>' + precio + '  euros </td><td></tr>'; 
+   
+    var tbody = document.getElementById("cuerpoTabla"); 
+    var producto = productos.find(prod => prod.id === id); 
+    var valorCarrito = document.getElementById("carritoTotal"); 
+    var boton = document.getElementById("botonadd" + producto.id); 
+
+    boton.innerHTML="Añadiendo..."; 
+    setTimeout(function() {
+      boton.innerHTML = "Añadir al carrito"; 
+    }, 1500);  
+
+    if (buscarProductoenCarrito(id) === true){
+       
+      
+      var cantidadProducto = document.getElementById("cantidadProd" + producto.id); 
+    
+      producto.cantidad += 1;
+      cantidadProducto.innerText = producto.cantidad; 
+
+    }
+    else {
+      
+      carrito.push(producto); 
+      var filaProducto = '<tr id="prod'+ producto.id + '"><td><img class="imgProd" src="' + producto.img + '"></td><td id="nombreProd">' + producto.nombre + '</td><td id="precioProd">' + producto.precio + '€ </td><td id="cantidadProd"><button class="botonCarrito" onclick="restarProd('+ producto.id + ')">-</button><spam id="cantidadProd'+ producto.id +'">' + producto.cantidad + '</spam><button class="botonCarrito" onclick="addMasProd('+ producto.id + ')">+</button>'  + '</td><td><img src="imagenes/x.png" class="imgCerrar" onclick="eliminarDelCarrito('+ producto.id + ')"></tr>'
       tbody.innerHTML += filaProducto; 
-    var total = document.getElementById("carritoTotal");
-    total.innerText = parseFloat(total.innerText) + parseFloat(precio);
+    }
+
+    const preciosTotales = carrito.map(prod => prod.precio * prod.cantidad); 
+    valorCarrito.innerText = preciosTotales.reduce((accumulator, currentValue) => accumulator + currentValue, 0); 
+    
+
+   
+}
+
+function addMasProd(id){
+  var valorCarrito = document.getElementById("carritoTotal"); 
+  var productoCarrito = carrito.find(prod => prod.id === id);
+  var cantidad = document.getElementById("cantidadProd" + productoCarrito.id);
+  productoCarrito.cantidad += 1; 
+  cantidad.innerText = productoCarrito.cantidad; 
+  const preciosTotales = carrito.map(prod => prod.precio * prod.cantidad); 
+  valorCarrito.innerText = preciosTotales.reduce((accumulator, currentValue) => accumulator + currentValue, 0); 
 }
 
 
-/*function comprobarDni(dni){
-var resto = numero %23;
 
-var letras = [‘T’, ‘R’, ‘W’, ‘A’, ‘G’, ‘M’, ‘Y’, ‘F’, ‘P’, ‘D’, ‘X’, ‘B’, ‘N’, ‘J’, ‘Z’, ‘S’, ‘Q’, ‘V’, ‘H’, ‘L’, ‘C’, ‘K’, ‘E’, ‘T’]; 
+function restarProd(id){
+  var valorCarrito = document.getElementById("carritoTotal"); 
+  var productoCarrito = carrito.find(prod => prod.id === id);
+  var cantidad = document.getElementById("cantidadProd" + productoCarrito.id);
 
-letras = letras.join("");
-
-var encontrado = letras.charAt(resto);
-
-if(letra_dni == encontrado) {
-alert("El dni insertado " + numero + "con letra " + letra_dni + " es CORRECTO");
+  if(productoCarrito.cantidad === 1){ //Cantidad minima del producto es 1
+    console.log("Cantidad mínima alcanzada")
+  }
+  else {
+    productoCarrito.cantidad -= 1; 
+    cantidad.innerText = productoCarrito.cantidad; 
+    const preciosTotales = carrito.map(prod => prod.precio * prod.cantidad); 
+    valorCarrito.innerText = preciosTotales.reduce((accumulator, currentValue) => accumulator + currentValue, 0); 
 }
-else{
-alert("El dni insertado " + numero + "con letra " + letra_dni + " es INCORRECTO");
+  }
+  
+
+function eliminarDelCarrito(id){
+  var valorCarrito = document.getElementById("carritoTotal"); //Global deberia ser
+  var indice = carrito.indexOf(carrito.find(prod => prod.id === id)); 
+  var productoCarrito = carrito.find(prod => prod.id === id);
+  var tableRow = document.getElementById("prod" + id);
+  tableRow.remove(); 
+  carrito.splice(indice, 1); 
+  productoCarrito.cantidad = 1; 
+  const preciosTotales = carrito.map(prod => prod.precio * prod.cantidad); 
+  valorCarrito.innerText = preciosTotales.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 }
 
-}*/
+////////////////////mover imagen
+window.onload = function(){
+  moverImagen();
+}
+function moverImagen(){
+  const imgElement = document.getElementById("mi-imagen"); // obtiene el elemento de imagen
+
+  let x = 0; // coordenada x de la imagen
+  let y = 0; // coordenada y de la imagen
+  let velocidadX = 10; // velocidad de movimiento en el eje x
+  let velocidadY = 10; // velocidad de movimiento en el eje y
+
+  function moverImagen() {
+    // obtiene el ancho y alto de la pantalla
+    const anchoPantalla = window.innerWidth;
+    const altoPantalla = window.innerHeight;
+
+    // obtiene el ancho y alto de la imagen
+    const anchoImagen = imgElement.offsetWidth;
+    const altoImagen = imgElement.offsetHeight;
+
+    // mueve la imagen en la dirección indicada por las velocidades
+    x += velocidadX;
+    y += velocidadY;
+
+    // si la imagen llega al borde de la pantalla, invierte la velocidad para que rebote
+    if (x + anchoImagen > anchoPantalla || x < 0) {
+      velocidadX = -velocidadX;
+    }
+    if (y + altoImagen > altoPantalla || y < 0) {
+      velocidadY = -velocidadY;
+    }
+
+    // actualiza la posición de la imagen
+    imgElement.style.top = y + "px";
+    imgElement.style.left = x + "px";
+  }
+
+  // mueve la imagen cada 10 milisegundos (10 veces por segundo)
+  const intervalId = setInterval(moverImagen, 10);
+
+  setTimeout(function() {
+    clearInterval(intervalId);
+    imgElement.style.display = "none";
+  }, 10000);
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ mainregistro.js
@@ -0,0 +1,168 @@
+//
+function validarDNI() {
+    var dni = document.getElementById("dni").value;
+    var letters = "TRWAGMYFPDXBNJZSQVHLCKE";
+  
+    if (dni.length != 9) {
+      var mensajeDNI = document.getElementById("mensajeDNI");
+      mensajeDNI.style.display = "inline"
+      return false;
+    }
+  
+    var number = dni.substring(0, 8);
+    var letter = dni.substring(8);
+  
+    if (isNaN(number)) {
+      var mensajeDNI = document.getElementById("mensajeDNI");
+      mensajeDNI.style.display = 'block'
+      return;
+    }
+  
+    if (letters[number % 23] != letter.toUpperCase()) {
+      var mensajeDNI = document.getElementById("mensajeDNI");
+      mensajeDNI.style.display = 'block'
+      return false;
+    }
+  
+    var mensajeDNI = document.getElementById("mensajeDNI");
+    mensajeDNI.style.display = 'none'
+    return true;
+  }
+  
+  //////////////////////
+  function validateAccount() {
+    var account = document.getElementById("numcuenta").value;
+  
+    if (account.length != 20) {
+      var mensajeCuenta = document.getElementById("mensajeCuenta");
+      mensajeCuenta.style.display = 'inline'
+      return false;
+    }
+  
+    if (isNaN(account)) {
+      var mensajeCuenta = document.getElementById("mensajeCuenta");
+      mensajeCuenta.style.display = 'inline'
+      return false;
+    }
+  
+    // Calculamos el dígito de control
+    var sum = 0;
+    for (var i = 0; i < account.length; i++) {
+      var digit = parseInt(account[i]);
+      if ((i + 1) % 2 == 1) {
+        // Los dígitos en posiciones impares se multiplican por 2 y se suman entre sí los dígitos del resultado
+        digit *= 2;
+        sum += digit % 10 + Math.floor(digit / 10);
+      } else {
+        // Los dígitos en posiciones pares se suman directamente
+        sum += digit;
+      }
+    }
+  
+    if (sum % 10 != 0) {
+      var mensajeCuenta = document.getElementById("mensajeCuenta");
+      mensajeCuenta.style.display = 'inline'
+      return false;
+    }
+  
+    var mensajeCuenta = document.getElementById("mensajeCuenta");
+    mensajeCuenta.style.display = 'none'
+    return true;
+  }
+  
+  ///////////////////////////
+  function validar() {
+  
+    validarDNI();
+    validarCuentaBancaria();
+    validarContraseña();
+  }
+  
+  
+  
+  
+  function validarCuentaBancaria() {
+  
+    let IBAN = document.getElementById('numcuenta').value;
+    //Se pasa a Mayusculas
+    IBAN = IBAN.toUpperCase();
+    //Se quita los blancos de principio y final.
+    IBAN = IBAN.trim();
+    IBAN = IBAN.replace(/\s/g, ""); //Y se quita los espacios en blanco dentro de la cadena
+    var letra1, letra2, num1, num2;
+    var isbanaux;
+    var numeroSustitucion;
+    //La longitud debe ser siempre de 24 caracteres
+    console.log(IBAN)
+    if (IBAN.length != 24) {
+      //alert('ERROR: IBAN incorrecto!);
+      var mensajeCuenta = document.getElementById("mensajeCuenta");
+      mensajeCuenta.style.display = 'inline'
+      return false;
+    }
+  
+    // Se coge las primeras dos letras y se pasan a números
+    letra1 = IBAN.substring(0, 1);
+    letra2 = IBAN.substring(1, 2);
+    num1 = getnumIBAN(letra1);
+    num2 = getnumIBAN(letra2);
+    //Se sustituye las letras por números.
+    isbanaux = String(num1) + String(num2) + IBAN.substring(2);
+    // Se mueve los 6 primeros caracteres al final de la cadena.
+    isbanaux = isbanaux.substring(6) + isbanaux.substring(0, 6);
+  
+    //Se calcula el resto, llamando a la función modulo97, definida más abajo
+    resto = modulo97(isbanaux);
+    if (resto == 1) {
+      var mensajeCuenta = document.getElementById("mensajeCuenta");
+      mensajeCuenta.style.display = 'none'
+      return true;
+    } else {
+      //alert('ERROR: IBAN incorrecto!);
+      var mensajeCuenta = document.getElementById("mensajeCuenta");
+      mensajeCuenta.style.display = 'inline'
+      return false;
+    }
+  }
+  
+  function modulo97(iban) {
+    var parts = Math.ceil(iban.length / 7);
+    var remainer = "";
+  
+    for (var i = 1; i <= parts; i++) {
+      remainer = String(parseFloat(remainer + iban.substr((i - 1) * 7, 7)) % 97);
+    }
+  
+    return remainer;
+  }
+  
+  function getnumIBAN(letra) {
+    ls_letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    return ls_letras.search(letra) + 10;
+  }
+  
+  
+  //validar conraseñas
+  
+  function validarContraseña() {
+    contraseña1 = document.getElementById("contraseña1");
+    contraseña2 = document.getElementById("contraseña2");
+  
+    // Verificamos si las constraseñas no coinciden
+    if (contraseña1.value != contraseña2.value) {
+  
+      var mensajeCuenta = document.getElementById("mensajeContraseña");
+      mensajeCuenta.style.display = 'inline'
+      return false;
+    }
+  
+    else {
+  
+      var mensajeCuenta = document.getElementById("mensajeContraseña");
+      mensajeCuenta.style.display = 'none'
+      return true;
+  
+     
+    }
+  }
+  
